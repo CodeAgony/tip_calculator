c5ae0f87a470ea731f549b9fadd1d1813075d1eb
CodeAgony <yaroslav.bkh@gmail.com>
2019-04-02 22:19:02 +0300

    Add servSatisfaction on click

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const initiate = function(){
-  document.getElementById('service').addEventListener('change', reportContent);
+  document.getElementById('service').addEventListener('change', servSatisfaction);
   document.getElementById('calc').addEventListener('click', doNothing);
 }
 
@@ -11,8 +11,27 @@ const doNothing = function(){
   console.log("do nothing");
 } 
 
-const reportContent = function(e){
-  console.log(e.target.value)
-}
+//Define a tip amount multiplier based on user choice
+const servSatisfaction = function(e){
+  switch(e.target.value) {
+    case 'excellent':
+      console.log("exc");
+      return 1.7
+    case 'great':
+      console.log("great");
+      return 1.5
+    case 'unsatisfactory':
+      console.log("unsatisfactory");
+      return 0.7
+    default: 
+      console.log("default");
+      return 1
+  }
+}   
+
+//Suggest a tip given the formula: (billamt * service) / people
+// const suggestTip(){
+//   let suggestion = (billamt.value * service) / people
+// }
 
 document.addEventListener('DOMContentLoaded', initiate);
