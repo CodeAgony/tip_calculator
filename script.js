187513bdc7dcb45e6f35e511aacbab0308b987f1
CodeAgony <yaroslav.bkh@gmail.com>
2019-04-02 18:20:12 +0300

    Add DOM references

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const initiate = function(){
   document.getElementById('calc').addEventListener('click', doNothing);
 }
 
+let billamt = document.getElementById('billamt');
+let service = document.getElementById('service');
+let people = document.getElementById('people');
+
 const doNothing = function(){
   console.log("do nothing");
 } 
 
-
 const reportContent = function(e){
   console.log(e.target.value)
 }
-
 
 document.addEventListener('DOMContentLoaded', initiate);
